9bb4e2052525291d05143634ac9495ca921cd7d0
Divyanshu <divyanshutiwari173@gmail.com>
2025-08-15 00:40:31 +0530

    finished about page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './component/Navbar'
 import Header from './component/Header'
+import About from './component/About'
 
 const page = () => {
   return (
     <div className='flex flex-col'>
     <Navbar/>
     <Header/>
+    <About/>
     </div>
   )
 }
